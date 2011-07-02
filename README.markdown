f934b04df5673b9d9927dcc1b127e28ef3e58fff
Jeffrey Kane <jbk@world.std.com>
2011-07-02 16:44:45 -0400

    Program After L5

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby Tutorial: sample application
+
+This is the sample application for [*Ruby on Rails Tutoria*] (http://railstutorial.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6a1ab518377c545951078662e6a82caf3d635eae3844da99b08fdb1cf5a394b0b04d6ceb5591518c05535a558f50421d3841234e35c444842abf7b1731379fd5'
